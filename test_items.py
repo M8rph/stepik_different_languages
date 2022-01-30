33ea7037137aa35b05204f61611c0871c2dbba80
M8rph <98570959+M8rph@users.noreply.github.com>
2022-01-30 17:41:02 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language(browser):
+def test_check_button(browser):
     browser.get(link)
     time.sleep(5)
     assert browser.find_elements_by_class_name("btn-add-to-basket"), "Button not found"
